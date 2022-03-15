b5ba34220bd6e539545ca7a3bf94009c9dcf73be
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-03-15 13:48:08 +0100

    about to add some JS

---
+++ main.js
@@ -0,0 +1,6 @@
+// get the value of the 'bill' and 'number of people' out of the input.
+// add eventlisteners to the percent button to multiply the corresponding percentage with the value gotten from the input and display the value on the h2 in the value section
+// to get the tip amount value:
+ //get the number value from  from the bill input, multiply it by the corresponding percent value, then divide it by the corresponding number value gotten from the number of people input.
+ // to get the total amount / person
+ // divide the bill by the number of people and add the tip.
